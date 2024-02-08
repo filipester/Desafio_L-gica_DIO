5017fe7c69b268a4aac7df0dcc102d41c477dda3
Filipe <filipester@gmail.com>
2024-02-08 14:35:38 -0300

    Escrevi a sequência de mensagens

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 // Código do desafio do felipao
 
+let listaHerois = [["Kratos", 9500],["Seya", 7000], ["Duke Nukem", 1500]]
+let nivelEpico = ""
 
+if (listaHerois[0][1] <= 1000) {
+    nivelEpico = "Ferro"
+}
+else if ((listaHerois[0][1] > 1000) && (listaHerois[0][1] <= 2000)) {
+    nivelEpico = "Bronze"
+}
+else if ((listaHerois[0][1] > 2000) && (listaHerois[0][1] <= 5000)) {
+    nivelEpico = "Prata"
+}
+else if ((listaHerois[0][1] > 5000) && (listaHerois[0][1] <= 7000)) {
+    nivelEpico = "Ouro"
+}
+else if ((listaHerois[0][1] > 7000) && (listaHerois[0][1] <= 8000)) {
+    nivelEpico = "Platina"
+}
+else if ((listaHerois[0][1] > 8000) && (listaHerois[0][1] <= 9000)) {
+    nivelEpico = "Ascendente"
+}
+else if ((listaHerois[0][1] > 9000) && (listaHerois[0][1] <= 10000)) {
+    nivelEpico = "Imortal"
+}
+else {
+    nivelEpico = "Radiante"
+}
+
+
+console.log("O herói de nome " + listaHerois[0][0] + " está no nível de " + nivelEpico)
